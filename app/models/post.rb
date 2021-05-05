87743f6975830a5ec161876289686d6f259d6f6d
francis uloko <francisuloko@yahoo.com>
2021-05-05 22:33:22 +0100

    Add Post association and validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ApplicationRecord
+  validates(:user_id, presence: {:message "Must be related with a user")
+  validates(:title, presence: {:message "Title is required"}, length: { in: 10..100 })
+  validates(:content, presence: {:message "Content can't be empty"}, length: { in: 140..3_140})
+
   belongs_to :user
+  has_many :comments
 end
